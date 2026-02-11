d3aba368b1e3301db4a60d586d0e2a4ff2459faf
mostafaboby123 <mostafa.boby123@gmail.com>
2026-02-12 01:24:59 +0200

    Update categoriesApis.js

--- src/redux/categories/categoriesApis.js
+++ src/redux/categories/categoriesApis.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const getCategories = async (type) => {
   try {
-    const res = await axios.get(`http://al3alamiabackend-production.up.railway.app/${type}`);
+    const res = await axios.get(`https://al3alamiabackend-production.up.railway.app/${type}`);
     return res.data;
   } catch (error) {
     console.log(error);
