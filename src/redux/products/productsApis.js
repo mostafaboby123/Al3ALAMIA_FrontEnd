468b8fb7a70794439daa6ad771bb6ca3d8a07828
mostafaboby123 <mostafa.boby123@gmail.com>
2026-02-12 01:23:15 +0200

    Update productsApis.js

--- src/redux/products/productsApis.js
+++ src/redux/products/productsApis.js
@@ -1,7 +1,7 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
-const URL = "http://al3alamiabackend-production.up.railway.app/products";
+const URL = "https://al3alamiabackend-production.up.railway.app/products";
 
 // Get all products
 const getProducts = async () => {
