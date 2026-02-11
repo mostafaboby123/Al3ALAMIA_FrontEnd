ddb3b1c7dd7f3bf9d4932f7f9dd7e1154526f998
mostafaboby123 <mostafa.boby123@gmail.com>
2026-02-12 01:09:11 +0200

    Update PersonalInformation.jsx

--- src/pages/profile/PersonalInformation/PersonalInformation.jsx
+++ src/pages/profile/PersonalInformation/PersonalInformation.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styles from "./personalInformation.module.css";
+import styles from "./PersonalInformation.module.css";
 import { updateUserDataApi } from "../../../redux/auth/authApis";
 import image from "../../../assets/user.png";
 
